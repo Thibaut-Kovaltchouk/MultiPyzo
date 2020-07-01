99661230a9cdd89a066a50922b0edfd58e45c3f8
Kovaltchouk <thibaut.kovaltchouk@gmail.com>
2020-07-01 22:54:08 +0200

    Conda + Pyzo portable simple

---
+++ miniconda3-win/shell/condabin/conda-hook.ps1
@@ -0,0 +1,8 @@
+$Env:CONDA_EXE = "C:/Users/kovaltchouk/miniconda3-win\Scripts\conda.exe"
+$Env:_CE_M = ""
+$Env:_CE_CONDA = ""
+$Env:_CONDA_ROOT = "C:/Users/kovaltchouk/miniconda3-win"
+$Env:_CONDA_EXE = "C:/Users/kovaltchouk/miniconda3-win\Scripts\conda.exe"
+
+Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1"
+Add-CondaEnvironmentToPrompt
